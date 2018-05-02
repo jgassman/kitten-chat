20bea6452a5f5c93fde02afcc7045b9b58499c69
jthatcher1293 <jthatcher1293@gmail.com>
2018-05-02 11:30:02 -0500

    add and style login page

---
+++ kittenchat/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+from chat import urls as chat_urls
+
+print(chat_urls)
+
+urlpatterns = [
+    url(r'^chat/', include(chat_urls)),
+    url(r'^login/$', auth_views.login),
+    url(r'^admin/', admin.site.urls),
+]
